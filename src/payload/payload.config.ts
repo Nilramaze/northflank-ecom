c56883c53cb26dfe46f32eaa4a67f4e4cdfa1ec8
Nilramaze <96390928+Nilramaze@users.noreply.github.com>
2024-02-20 15:39:36 +0100

    port fix

--- src/payload/payload.config.ts
+++ src/payload/payload.config.ts
@@ -40,6 +40,11 @@ dotenv.config({
 })
 
 export default buildConfig({
+  
+  serverURL: process.env.PAYLOAD_PUBLIC_BASE_DNS,
+  rateLimit: {
+    trustProxy: true,
+  },
   admin: {
     user: Users.slug,
     bundler: webpackBundler(), // bundler-config
@@ -80,7 +85,6 @@ export default buildConfig({
     url: process.env.DATABASE_URI,
   }),
   // database-adapter-config-end
-  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   collections: [Pages, Products, Orders, Media, Categories, Users],
   globals: [Settings, Header, Footer],
   typescript: {
